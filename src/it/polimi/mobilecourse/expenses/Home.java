e6ad7e118ed96e293eb68af32259e8b71b9996d0
accateo <matteo.crc@gmail.com>
2014-11-16 16:38:26 +0100

    aggiornamento classe Home e Manifest

--- src/it/polimi/mobilecourse/expenses/Home.java
+++ src/it/polimi/mobilecourse/expenses/Home.java
@@ -1,22 +1,34 @@
 package it.polimi.mobilecourse.expenses;
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.*;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
 
 
 
-public class Home extends Activity {
+public class Home extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.fragment_home);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.top_fragment, new PlaceholderFragment())
+                    .commit();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.bottom_fragment, new BottomFragment())
                     .commit();
         }
     }
@@ -52,8 +64,33 @@ public class Home extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            View rootView = inflater.inflate(R.layout.top_fragment, container, false);
+
+            RadioButton radio=(RadioButton)rootView.findViewById(R.id.radioButton);
+
             return rootView;
         }
     }
+
+    public static class BottomFragment extends Fragment {
+
+        public BottomFragment() {
+
+
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View bottomView = inflater.inflate(R.layout.bottom_fragment, container, false);
+
+            Button prova=(Button)bottomView.findViewById(R.id.button);
+
+
+            return bottomView;
+        }
+    }
+
+
 }
