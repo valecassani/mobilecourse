d6ce2def1907ca25be82f46158bab8a161a3f622
accateo <matteo.crc@gmail.com>
2014-11-16 21:01:18 +0100

    creazioni classi ad hoc per i fragment della Home

--- src/it/polimi/mobilecourse/expenses/Home.java
+++ src/it/polimi/mobilecourse/expenses/Home.java
@@ -9,15 +9,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.*;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.RadioButton;
+import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
-
-public class Home extends FragmentActivity {
+public class Home extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +24,26 @@ public class Home extends FragmentActivity {
         setContentView(R.layout.fragment_home);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.top_fragment, new PlaceholderFragment())
+                    .add(R.id.top_fragment, new TopFragmentHome())
                     .commit();
             getFragmentManager().beginTransaction()
-                    .add(R.id.bottom_fragment, new BottomFragment())
+                    .add(R.id.bottom_fragment, new BottomFragmentHome())
                     .commit();
+            ListView lista=(ListView)findViewById(R.id.listAction);
+            String [] actions=new String[]{"controlla conto"};
+            ArrayList<String> azioni=new ArrayList<String>();
+            azioni.addAll(Arrays.asList(actions));
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.simplerow,azioni);
+
+            lista.setAdapter(arrayAdapter);
+            lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                }
+            });
         }
+
     }
 
 
@@ -53,44 +66,9 @@ public class Home extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.top_fragment, container, false);
-
-            RadioButton radio=(RadioButton)rootView.findViewById(R.id.radioButton);
-
-            return rootView;
-        }
-    }
-
-    public static class BottomFragment extends Fragment {
-
-        public BottomFragment() {
 
 
 
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View bottomView = inflater.inflate(R.layout.bottom_fragment, container, false);
-
-            Button prova=(Button)bottomView.findViewById(R.id.button);
-
-
-            return bottomView;
-        }
-    }
 
 
 }
