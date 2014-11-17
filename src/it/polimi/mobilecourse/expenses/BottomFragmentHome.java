20ed623f8358fafeb3d59c51f7634f0337eb6919
accateo <matteo.crc@gmail.com>
2014-11-17 14:20:43 +0100

    creazioni classi ad hoc per i fragment della Home

--- src/it/polimi/mobilecourse/expenses/BottomFragmentHome.java
+++ src/it/polimi/mobilecourse/expenses/BottomFragmentHome.java
@@ -5,14 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Matteo on 16/11/2014.
  */
 public class BottomFragmentHome extends Fragment {
+
+    private String[] actions;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View bottomView = inflater.inflate(R.layout.bottom_fragment, container, false);
+        String [] actions=new String[]{"controlla conto"};
+        List<String> azioni=new ArrayList<String>();
+        azioni.addAll(Arrays.asList(actions));
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,azioni);
+        ListView lista=(ListView)bottomView.findViewById(R.id.listAction);
+
+        lista.setAdapter(arrayAdapter);
 
 
         return bottomView;
