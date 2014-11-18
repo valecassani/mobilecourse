4ea15c9294ff14fa5508d3700e399757fd2fb3aa
accateo <matteo.crc@gmail.com>
2014-11-18 16:27:28 +0100

    tasto indietro NewOperation

--- src/it/polimi/mobilecourse/expenses/NewOperation.java
+++ src/it/polimi/mobilecourse/expenses/NewOperation.java
@@ -1,7 +1,10 @@
 package it.polimi.mobilecourse.expenses;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Matteo on 17/11/2014.
@@ -11,5 +14,15 @@ public class NewOperation extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_operation);
+        ImageView back=(ImageView)findViewById(R.id.imageView);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent myintent = new Intent(view.getContext(), Home.class);
+                startActivity(myintent);
+            }
+        });
+
     }
 }
