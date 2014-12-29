41980822da3ae3d5124eae2f676534e24789d810
valecassani <valerio.cassani@gmail.com>
2014-12-29 16:24:31 +0100

    Altri lavori di integrazione

--- src/it/polimi/mobilecourse/expenses/LoginTutor.java
+++ src/it/polimi/mobilecourse/expenses/LoginTutor.java
@@ -1,7 +1,10 @@
 package it.polimi.mobilecourse.expenses;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Matteo on 23/12/2014.
@@ -11,5 +14,20 @@ public class LoginTutor extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_tutor);
+
+        Button butS=(Button) findViewById(R.id.buttonLogin);
+        butS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myintent = new Intent(v.getContext(), HomeTutor.class);
+                startActivity(myintent);
+
+            }
+        });
+
+
     }
+
+
 }
