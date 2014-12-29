4c1be37caaf48620adbd7622d6231ad0c985c001
valecassani <valerio.cassani@gmail.com>
2014-12-29 12:39:15 +0100

    Prova commit da android studio

--- src/it/polimi/mobilecourse/expenses/HomeTutor.java
+++ src/it/polimi/mobilecourse/expenses/HomeTutor.java
@@ -1,26 +1,34 @@
 package it.polimi.mobilecourse.expenses;
 
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Home extends Activity {
+public class HomeTutor extends ActionBarActivity {
 
     public static final String TAG = "Money Notes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setContentView(R.layout.fragment_home);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
         if (savedInstanceState == null) {
             MidFragmentHome mf=new MidFragmentHome();
             getFragmentManager().beginTransaction().add(R.id.layoutreplace,new TopFragmentHome()).commit();
             getFragmentManager().beginTransaction().add(R.id.secondreplace,mf).commit();
 
         }
+
+
 
     }
 
@@ -35,7 +43,7 @@ public class Home extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the HomeTutor/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
