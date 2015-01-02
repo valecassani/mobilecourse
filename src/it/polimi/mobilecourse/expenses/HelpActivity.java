f58bf07c6006401183b4d660b0f8deb62abd2e9e
valecassani <valerio.cassani@gmail.com>
2015-01-02 15:39:35 +0100

    tentativo di query user data

--- src/it/polimi/mobilecourse/expenses/HelpActivity.java
+++ src/it/polimi/mobilecourse/expenses/HelpActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 
 import java.util.ArrayList;
 
 /**
  * Created by Matteo on 22/12/2014.
  */
-public class HelpActivity extends Activity {
+public class HelpActivity extends ActionBarActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
