8a96d6d53a776ff7d08f03a051f4298608ddb746
accateo <matteo.crc@gmail.com>
2015-01-07 18:35:34 +0100

    Login studente con tasto FB

--- src/it/polimi/mobilecourse/expenses/LoginStudente.java
+++ src/it/polimi/mobilecourse/expenses/LoginStudente.java
@@ -2,6 +2,7 @@ package it.polimi.mobilecourse.expenses;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -10,17 +11,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
 /**
  * Created by Matteo on 23/12/2014.
  */
 public class LoginStudente extends ActionBarActivity {
+
+    boolean logged;
+    Context ctx;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_studente);
 
 
-        Button butS = (Button) findViewById(R.id.buttonLogin);
+        //Button butS = (Button) findViewById(R.id.buttonLogin);
 /*        butS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +42,42 @@ public class LoginStudente extends ActionBarActivity {
             }
         });*/
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        Session.getActiveSession().onActivityResult(this,requestCode,resultCode,data);
+        facebookLogin();
+
+    }
+
+    public void facebookLogin(){
+        final Intent myintent = new Intent(getApplicationContext(), DataActivityStudent.class);
+
+
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+                        @Override
+                        public void onCompleted(final GraphUser user, final Response response) {
+                            if (user != null) {
+                                logged = true;
+                                startActivity(myintent);
+
+                            }
+                        }
+                    }).executeAsync();
+
+                }
+                else
+                {
+                    logged = false;
+                }
+            }});
     }
 
 
